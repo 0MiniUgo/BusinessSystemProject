3e33aea95bf5e2ac2c0d7e8fbc23585acb484098
0MiniPorrinha <hugorodrigues969@gmail.com>
2022-07-26 14:35:44 -0300

    First commit

---
+++ src/main/java/com/hugo/businesssystem/BusinessSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.businesssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessSystemApplication.class, args);
+    }
+
+}
